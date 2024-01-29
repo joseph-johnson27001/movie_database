c855362f62e5be6a25fc60d6da2a23b6240329f8
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2024-01-29 11:33:47 +0700

    Navbar and page layout

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,55 @@
 <template>
-  <div></div>
+  <div id="app">
+    <NavBar />
+    <main>
+      <HomeView />
+    </main>
+  </div>
 </template>
 
 <script>
+import NavBar from "./components/NavBar.vue";
+import HomeView from "./views/HomePage.vue";
+
 export default {
   name: "App",
+  components: {
+    NavBar,
+    HomeView,
+  },
 };
 </script>
 
 <style>
-#app {
+body {
+  background-color: #f7f7f7;
+  margin: 0;
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+}
+
+#app {
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+p,
+li,
+h1,
+h2,
+h3 {
+  color: white !important;
+}
+
+strong {
+  color: #2c3e50;
+}
+
+main {
+  max-width: 1200px;
+  margin: 0px auto;
+  padding: 20px;
+  background-color: white;
+  border: 1px solid #ddd;
+  min-height: 100vh;
 }
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,39 @@
+<template>
+  <nav>
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">New Releases</a></li>
+      <!-- Add more navigation links as needed -->
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+};
+</script>
+
+<style scoped>
+/* Add your navbar styling here */
+nav {
+  background-color: #333;
+  color: white;
+  padding: 10px 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline;
+  margin-right: 20px;
+}
+
+a {
+  text-decoration: none;
+  color: white;
+}
+</style>
